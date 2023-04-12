c89731aba1969c7a97b9cd2064053f7b8c705926
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-04-12 22:08:54 +0200

    Port F header file

---
+++ SysTick Timer Lecture bonus/tm.h
@@ -0,0 +1,30 @@
+/*
+ *
+ *@ Author: Ahmed Bakr
+ *  Date: 12/4/2023
+ *	7 seconds counter (lecture bonus)
+ *
+ */
+
+//*****************************************************************************
+//
+// GPIO registers (PORTF)
+//
+//*****************************************************************************
+
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+
+// Prototypes
+void PortF_Init(void);
+void RGB(unsigned char colour);
+
+/************** END OF FILE *****************/
