4d5d49e112ebb9c26e7cf177a34b2176faf11056
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-04-12 22:22:04 +0200

    Update SysTick.c

--- SysTick Timer Lecture bonus/SysTick.c
+++ SysTick Timer Lecture bonus/SysTick.c
@@ -15,7 +15,7 @@
 /******************************************/
 void SysTick_Init(void){
 	NVIC_ST_CTRL_R = 0;
-	NVIC_ST_RELOAD_R = 800000;
+	NVIC_ST_RELOAD_R = 800000-1;
 	NVIC_ST_CURRENT_R = 0;
 	NVIC_ST_CTRL_R = 5;
 }
